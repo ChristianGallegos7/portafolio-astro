d3c141f0bdf37a2b1bbdfd64ffbb27dc2e0f399e
Lancelot <lancelot@sunset.es>
2024-08-28 15:16:14 -0500

    Tecnologias ordenadas

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop'
+    }
+  },
   integrations: [tailwind()]
 });
